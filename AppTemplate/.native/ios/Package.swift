3de449f168051e090bfa78a97868dac5365659f2
its-ernest <ernestdior5050@gmail.com>
2026-08-22 00:57:30 +0000

    add: ios ui plugin

--- AppTemplate/.native/ios/Package.swift
+++ AppTemplate/.native/ios/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "GenericApp",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "App", targets: ["App"])
